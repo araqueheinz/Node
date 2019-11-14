c60391cb4d66f7f2c32ab7f246643ae303233e0f
Heinz Araque <araqueheinz@gmail.com>
2019-11-14 08:22:28 -0500

    used validor npm

--- 01_ModuleSystem/app.js
+++ 01_ModuleSystem/app.js
@@ -8,6 +8,9 @@ const getNotes = require('./notes');
 // https://www.npmjs.com/package/validator
 const validator = require('validator');
 
+// https://www.npmjs.com/package/chalk
+const chalk = require('chalk');
+
 fs.writeFileSync('notes.txt', 'This file was created by Node.js');
 
 /* 
